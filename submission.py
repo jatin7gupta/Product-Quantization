9db3e469631c7a39b06d63f5e24fb4d6c947869a
jatin7gupta <guptajatin@outlook.com>
2020-04-13 18:30:39 +1000

    getting distance between a part of dims is complete

--- submission.py
+++ submission.py
@@ -1,5 +1,16 @@
+from scipy.spatial import distance
 def pq(data, P, init_centroids, max_iter):
-    pass
+    # constant variables
+    ROW_LENGTH, COL_LENGTH = data.shape
+
+    # dividing dimensions of data into p parts (M/P from spec)
+    size_of_division = COL_LENGTH // P
+    for index in range(0, COL_LENGTH, size_of_division):
+        # To get P as centroid index
+        centriod_index = index//size_of_division
+        ans = distance.cdist(data[:, index:index+size_of_division], init_centroids[centriod_index], 'cityblock')
+        print(ans)
+
 
 def query(queries, codebooks, codes, T):
     pass
--- test.py
+++ test.py
@@ -12,14 +12,14 @@ codebooks, codes = submission.pq(Data_File, P=2, init_centroids=Centroids_File, 
 end = time.time()
 time_cost_1 = end - start
 
-
-# How to run your implementation for Part 2
-with open('./toy_example/Query_File', 'rb') as f:
-    queries = pickle.load(f, encoding = 'bytes')
-start = time.time()
-candidates = submission.query(queries, codebooks, codes, T=10)
-end = time.time()
-time_cost_2 = end - start
-
-# output for part 2.
-print(candidates)
+#
+# # How to run your implementation for Part 2
+# with open('./toy_example/Query_File', 'rb') as f:
+#     queries = pickle.load(f, encoding = 'bytes')
+# start = time.time()
+# candidates = submission.query(queries, codebooks, codes, T=10)
+# end = time.time()
+# time_cost_2 = end - start
+#
+# # output for part 2.
+# print(candidates)
