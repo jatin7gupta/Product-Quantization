e1f9be3e482031744fedf51b4dc621aac9fefacb
jatin7gupta <guptajatin@outlook.com>
2020-04-15 21:37:32 +1000

    ques 1 done

--- submission.py
+++ submission.py
@@ -10,25 +10,49 @@ def pq(data, P, init_centroids, max_iter):
 
     # dividing dimensions of data into p parts (M/P from spec)
     size_of_division = COL_LENGTH // P
-    codebook = np.zeros((ROW_LENGTH, K))
+    codebook_distance_sum = np.zeros((ROW_LENGTH, K))
 
     # key: cluster number
     # value: cluster points in key cluster
-    cluster = defaultdict(list)
+    cluster = None
+    while max_iter > 0:
+        cluster = defaultdict(list)
+        codebook_distance_sum = np.zeros((ROW_LENGTH, K))
+
+        for index in range(0, COL_LENGTH, size_of_division):
+            # To get P as centroid index
+            codebook_index = index//size_of_division
+
+            # using l1 distance as cityblock
+            codebook_distance_sum += distance.cdist(data[:, index:index+size_of_division], init_centroids[codebook_index], 'cityblock')
+
+        for idx, value in enumerate(np.argmin(codebook_distance_sum, axis=1)):
+            cluster[value].append(idx)
+
+        for cluster_key, data_points in cluster.items():
+            for index in range(0, COL_LENGTH, size_of_division):
+                codebook_index = index // size_of_division
+                init_centroids[codebook_index][cluster_key] = \
+                    np.mean(data[data_points, index:index+size_of_division], axis=0)
+
+        max_iter = max_iter-1
+
+    codes = None
+    first_time = True
     for index in range(0, COL_LENGTH, size_of_division):
         # To get P as centroid index
-        codebook_index = index//size_of_division
+        codebook_index = index // size_of_division
 
         # using l1 distance as cityblock
-        codebook += distance.cdist(data[:, index:index+size_of_division], init_centroids[codebook_index], 'cityblock')
+        codes_distances = distance.cdist(data[:, index:index + size_of_division], init_centroids[codebook_index],
+                                         'cityblock')
+        if first_time:
+            codes = np.argmin(codes_distances, axis=1)
+            first_time = False
+        else:
+            codes = np.column_stack((codes, np.argmin(codes_distances, axis=1)))
+    return init_centroids, codes.astype(np.uint8)
 
-    for idx, value in enumerate(np.argmin(codebook, axis=1)):
-        cluster[value].append(idx)
-
-    for cluster_key, data_points in cluster.items():
-        for index in range(0, COL_LENGTH, size_of_division):
-            codebook_index = index // size_of_division
-            init_centroids[codebook_index][cluster_key]  = np.mean(data[data_points, index:index+size_of_division], axis=0)
 
 
 def query(queries, codebooks, codes, T):
--- test.py
+++ test.py
@@ -7,10 +7,18 @@ with open('./toy_example/Data_File', 'rb') as f:
     Data_File = pickle.load(f, encoding = 'bytes')
 with open('./toy_example/Centroids_File', 'rb') as f:
     Centroids_File = pickle.load(f, encoding = 'bytes')
+l=[]
+# for i in range(100):
 start = time.time()
 codebooks, codes = submission.pq(Data_File, P=2, init_centroids=Centroids_File, max_iter = 20)
 end = time.time()
 time_cost_1 = end - start
+#     l.append(time_cost_1)
+# print(sum(l)/len(l))
+# print(f'min={min(l)}')
+# print(f'max={max(l)}')
+print(codebooks)
+print(codes)
 
 #
 # # How to run your implementation for Part 2
